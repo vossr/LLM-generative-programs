e68cfaa3552b75c1f2450140fd4626908af57f4c
vossr <pehkonenroni@gmail.com>
2024-09-15 13:18:19 +0300

    generate laws by country table

---
+++ write_jurisdictional_legal_analysis.py
@@ -0,0 +1,39 @@
+import python_interface
+import datetime
+import time
+import csv
+
+start_time_unix_ms = int(time.time() * 1000)
+countries = open("jurisdictional_table/country_names.txt").read().split('\n')
+print(countries)
+laws = python_interface.generate_list("Write a list of controversial laws around the world")
+laws += python_interface.generate_list("Write a long list of controversial laws around the world (subject to debate), diverse range")
+# laws = python_interface.generate_list("Write a list of laws around the world with contentious issues, laws are subject to intense public debate and differing opinions across various political, cultural, and national lines")
+for i in range(len(laws)):
+    laws[i] = laws[i].split(":")[0]
+    if 'vs.' in laws[i]:
+        laws[i] = laws[i].split("vs.")[1]
+    laws[i] = laws[i].strip()
+print(laws)
+res = [["Intended as parody and/or educational on generative llms"], [' '], [' ']]
+res.append([""] + countries)
+for L in laws:
+    print(L, end="", flush=True)
+    line = [L]
+    for c in countries:
+        print(" ", c, end="", flush=True)
+        # line.append(python_interface.boolean_question(f"In {c} is {L} generally supported"))
+        line.append(python_interface.boolean_question(f"In {c} is {L} endorsed by law"))
+    print("\n", line)
+    res.append(line)
+
+end_time_unix_ms = int(time.time() * 1000)
+formatted_time = str(datetime.timedelta(seconds=int(end_time_unix_ms - start_time_unix_ms) / 1000))[:-7]
+time_str = "Chat with RTX Mistral took " + formatted_time + " to generate this"
+res.append([' '])
+res.append([time_str])
+res.insert(1, [time_str])
+
+with open('jurisdictional_table/jurisdictional_table.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(res)
